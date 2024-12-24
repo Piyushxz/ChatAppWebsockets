52c9e685e6bcb0eb442dba828ba0440a837de61a
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-12-25 00:19:22 +0530

    all socket logic

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const ws = new ws_1.WebSocketServer({ port: 8080 });
+const sockets = [];
 ws.on("connection", (socket) => {
-    socket.send("Hey");
+    sockets.push(socket);
+    socket.on("message", (message) => {
+        for (let i = 0; i < sockets.length; i++) {
+            sockets[i].send(message.toString());
+        }
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer ,WebSocket} from "ws";
 
 
 const ws = new WebSocketServer({port:8080})
 
 
+const sockets:WebSocket[] = []
+
 ws.on("connection",(socket)=>{
 
-    socket.send("Hey")
+    sockets.push(socket)
+
+
+
+    socket.on("message",(message)=>{
+        for(let i = 0 ; i < sockets.length ; i++){
+            sockets[i].send(message.toString())
+        }
+    })
 })
