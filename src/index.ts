678dedbca320db308328a4ce9ee0c62fe2d26b95
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-12-25 12:55:03 +0530

    room logiv

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,40 @@
 import { WebSocketServer ,WebSocket} from "ws";
 
+interface User{
+    socket:WebSocket,
+    room:string
+}
 
 const ws = new WebSocketServer({port:8080})
 
 
-const sockets:WebSocket[] = []
+
+const sockets:User[] = []
 
 ws.on("connection",(socket)=>{
 
-    sockets.push(socket)
-
-
-
     socket.on("message",(message)=>{
-        for(let i = 0 ; i < sockets.length ; i++){
-            sockets[i].send(message.toString())
+        //@ts-ignore
+        const parsedMessage = JSON.parse(message)
+
+        if(parsedMessage.type === "join"){
+            sockets.push({
+                socket,
+                room:parsedMessage.payload.roomId
+            })
+        }
+
+
+        if(parsedMessage.type == "chat"){
+            const currentUserRoom = sockets.find((x)=>x.socket == socket)?.room
+
+            for(let i = 0 ; i < sockets.length;i++){
+                if(sockets[i].room === currentUserRoom){
+                    sockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }
         }
     })
+
+    
 })
